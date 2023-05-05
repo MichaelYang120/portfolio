3fbdbd32c4dc8ccb50f7989e0551e31dcea22aa4
MichaelYang120 <michaelyangiscoding@gmail.com>
2023-05-05 18:33:34 -0500

    added functionality to Main.tsx

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -1,12 +1,28 @@
-import React, { useEffect, useState, ChangeEvent } from "react";
+import React from "react";
 
 export default function Main() {
+
 	const mainstyles = {
 		height: "100vh",
+		// display: "grid"
 	}
+
+	const infoboxstlye = {
+		background: "purple",
+		padding: "80px 50px",
+		margin: "5px 5px"
+	}
+
+	const infotext = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta suscipit cupiditate animi minus expedita, commodi doloribus voluptas beatae ab incidunt earum consequatur non maiores, reiciendis iste voluptatum quidem unde laboriosam officia quas quasi. Maiores sunt velit iure accusamus accusantium deserunt adipisci necessitatibus ipsa nulla nisi rem pariatur, facilis debitis vel!"
+
+	const infotext2 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta suscipit cupiditate animi minus expedita, commodi doloribus voluptas beatae ab incidunt earum consequatur non maiores, reiciendis iste voluptatum quidem unde laboriosam officia quas quasi. Maiores sunt velit iure accusamus accusantium deserunt adipisci necessitatibus ipsa nulla nisi rem pariatur, facilis debitis vel!"
+
 	return (
-		<div style={mainstyles}>
-            this is a test
-		</div>
+		<>
+			<div style={mainstyles}>
+				<div style={infoboxstlye}>{infotext}</div>
+				<div style={infoboxstlye}>{infotext2}</div>
+			</div>
+		</>
 	);
 }

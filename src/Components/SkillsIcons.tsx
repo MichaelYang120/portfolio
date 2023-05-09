c409d789642c799c60ba0c408bb17320580a4f6b
MichaelYang120 <michaelyangiscoding@gmail.com>
2023-05-08 21:56:07 -0500

    added icon

--- src/Components/SkillsIcons.tsx
+++ src/Components/SkillsIcons.tsx
@@ -1,10 +1,28 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMugSaucer } from '@fortawesome/free-solid-svg-icons'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faReact } from '@fortawesome/free-brands-svg-icons'
+import {
+    IconLookup,
+    IconDefinition,
+    findIconDefinition
+  } from '@fortawesome/fontawesome-svg-core'
+  
+  export function SkillsIcons() {
+    const coffeeLookup: IconLookup = { prefix: 'fas', iconName: 'coffee' }
+    const coffeeIconDefinition: IconDefinition = findIconDefinition(coffeeLookup)
 
-export function SkillsIcons() {
-	
     return (
         <>
-            test
+            <div>
+                {/* <FontAwesomeIcon icon={faCoffee} />
+                <FontAwesomeIcon icon={faMugSaucer} /> */}
+                <FontAwesomeIcon icon={coffeeIconDefinition} />
+                <FontAwesomeIcon icon={faReact} />
+
+
+            </div>
         </>	
     )
 }

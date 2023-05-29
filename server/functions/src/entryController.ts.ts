329c10d1ffaf2e188089409a082b5610aee0cb03
MichaelYang120 <michaelyangiscoding@gmail.com>
2023-05-29 01:10:19 -0500

    added update and delete functions

--- server/functions/src/entryController.ts.ts
+++ server/functions/src/entryController.ts.ts
@@ -18,12 +18,13 @@ const addEntry = async (req: Request, res: Response) => {
     const entry = db.collection("entries").doc();
     const entryObject = {
       id: entry.id,
+      timestamp: new Date,
       title,
       text,
     };
 
-    entry.set(entryObject);
-    // await entry.set(entryObject);
+    // entry.set(entryObject);
+    await entry.set(entryObject);
 
     res.status(200).send({
       status: "success",
@@ -46,4 +47,55 @@ const getAllEntries = async (req: Request, res: Response) => {
   }
 };
 
-export {addEntry, getAllEntries};
+const updateEntry = async (req: Request, res: Response) => {
+  const {body: {text, title}, params: {entryId}} = req;
+
+  try {
+    const entry = db.collection("entries").doc(entryId);
+    const currentData = (await entry.get()).data() || {};
+
+    const entryObject = {
+      title: title || currentData.title,
+      text: text || currentData.text,
+    };
+
+    await entry.set(entryObject).catch((error) => {
+      return res.status(400).json({
+        status: "error",
+        message: error.message,
+      });
+    });
+
+    return res.status(200).json({
+      status: "success",
+      message: "entry updated successfully",
+      data: entryObject,
+    });
+  } catch (error:any) {
+    return res.status(500).json(error.message);
+  }
+};
+
+const deleteEntry = async (req: Request, res: Response) => {
+  const {entryId} = req.params;
+
+  try {
+    const entry = db.collection("entries").doc(entryId);
+
+    await entry.delete().catch((error) => {
+      return res.status(400).json({
+        status: "error",
+        message: error.message,
+      });
+    });
+
+    return res.status(200).json({
+      status: "success",
+      message: "entry deleted successfully",
+    });
+  } catch (error:any) {
+    return res.status(500).json(error.message);
+  }
+};
+
+export {addEntry, getAllEntries, updateEntry, deleteEntry};
--- server/functions/src/index.ts
+++ server/functions/src/index.ts
@@ -19,12 +19,19 @@
 // });
 import * as functions from "firebase-functions";
 import * as express from "express";
-import {addEntry, getAllEntries} from "./entryController.ts";
+import {
+  addEntry,
+  getAllEntries,
+  updateEntry,
+  deleteEntry,
+} from "./entryController.ts";
 
 const app = express();
 
 app.get("/", (req, res) => res.status(200).send("Silence is Golden"));
 app.post("/entries", addEntry);
 app.get("/entries", getAllEntries);
+app.patch("/entries/:entryId", updateEntry);
+app.delete("/entries/:entryId", deleteEntry);
 
 exports.app = functions.https.onRequest(app);

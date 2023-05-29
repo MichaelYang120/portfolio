b567f762f97cf10788b86aa47c772d10dce1460c
MichaelYang120 <michaelyangiscoding@gmail.com>
2023-05-29 11:23:45 -0500

    added post method to Blog.tsx

--- client/src/Components/Blog.tsx
+++ client/src/Components/Blog.tsx
@@ -2,9 +2,41 @@ import React from "react";
 
 export default function Blog() {
 
+	const submitText = (event:React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		console.log(event)
+		async function postJSON(data:any) {
+			try {
+			  const response = await fetch("https://us-central1-portfolio-f4982.cloudfunctions.net/app/entries", {
+				mode: 'no-cors',
+				method: "POST", // or 'PUT'
+				headers: {
+				  "Content-Type": "application/json",
+				  'Accept': 'application/json, text/plain, */*',
+				},
+				body: JSON.stringify(data),
+			  });
+			  console.log(JSON.stringify(data))
+		  
+			  const result = await response.json();
+			  console.log("Success:", result);
+			} catch (error) {
+			  console.error("Error:", error);
+			}
+		  }
+		  
+		  const data = { title: "", text: "" };
+		  postJSON(data);
+	}
 	return (
 			<>
-			this is blog area
+				<div>
+					<form onSubmit={submitText} >
+						<input type="text" id="title" placeholder="title" />
+						<input type="text" id="text" placeholder="text" />
+						<input type="submit" id="textsubmit" placeholder="Submit"/>
+					</form>
+				</div>
 			</>
 		)
 
